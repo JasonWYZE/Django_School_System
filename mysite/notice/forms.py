fb297ce0683088e33e769f485d91321aa8f0fd77
JasonWYZE <jason.ze41@gmail.com>
2022-04-19 12:19:59 -0400

    update

--- mysite/notice/forms.py
+++ mysite/notice/forms.py
@@ -2,12 +2,6 @@ from django.forms import ModelForm
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
- 
-class createuserform(UserCreationForm):
-    class Meta:
-        model=User
-        fields=['username','password'] 
- 
 
 class addNoticeform(ModelForm):
     class Meta:
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,4 +2,4 @@ from django import views
 from django.urls import path
 from . import views
 
-urlpatterns = [ path('/polls',views.index,name='index'),]
+urlpatterns = [ path('',views.index,name='index'),]
