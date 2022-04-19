584abebe6ad58c87dad4d72fe321ab4d5ee10fff
JasonWYZE <jason.ze41@gmail.com>
2022-04-19 11:42:23 -0400

    update

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,4 +2,4 @@ from django import views
 from django.urls import path
 from . import views
 
-urlpatterns = [ path('',views.index,name='index'),]
+urlpatterns = [ path('/polls',views.index,name='index'),]
